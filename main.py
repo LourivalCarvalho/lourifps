455dc5dd21fe293f2e533107863b49c4ba23154c
LourivalCarvalho <carvalho.lourival.junior@gmail.com>
2024-08-23 17:55:52 -0300

    [200~Added stylish buttons, bug fixes, music, and sound effects

--- main.py
+++ main.py
@@ -2,67 +2,47 @@ import pygame
 import random
 import time
 import shot_particles
+import sys
 
-# Inicializando o Pygame
 pygame.init()
 
-# Configurações da tela
+# Screen settings
 screen_width = 1280
 screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("FPS Game")
 
-# Definição de cores
+# Color definitions
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-# Carregamento de fontes
-font = pygame.font.Font(None, 48)
+# Font loading
+font = pygame.font.Font(None, 56)
 
-# Carregamento de imagens
+# Image loading
 target_image = pygame.image.load('assets/target.png').convert_alpha()
 aim_image = pygame.image.load('assets/aim.png').convert_alpha()
 background_image = pygame.image.load('assets/bg_menu.jpg').convert()
 
-# Definição de sons
-shot_sound = pygame.mixer.Sound('assets/shot1.mp3')
+# Sound definitions
+shot_sounds = [
+    pygame.mixer.Sound('assets/shot1.mp3'),
+    pygame.mixer.Sound('assets/shot2.mp3'),
+    pygame.mixer.Sound('assets/shot3.mp3')
+]
 hit_sound = pygame.mixer.Sound('assets/shot3.mp3')
 reload_sound = pygame.mixer.Sound('assets/reload.mp3')
 
-# Crie o sistema de partículas
+# Create the particle system
 particle_system = shot_particles.ParticleSystem()
 
-'''# Função para o efeito de partículas ao atirar
-def create_particles_shot(pos, screen):
-    particles = []
-    for _ in range(7):
-        velocity = [random.randint(-5, 5), random.randint(-5, 5)]
-        particles.append([list(pos), velocity])
-    for particle in particles:
-        particle[0][0] += particle[1][0]
-        particle[0][1] += particle[1][1]
-        particle[1][1] += 0.1  # Gravidade
-        pygame.draw.circle(screen, white, [int(particle[0][0]), int(particle[0][1])], 3)'''
-
-'''# Função para o efeito de partículas ao explodir o alvo
-def create_particles_explode(pos, screen):
-    particles = []
-    for _ in range(15):
-        velocity = [random.randint(-7, 7), random.randint(-7, 7)]
-        particles.append([list(pos), velocity])
-    for particle in particles:
-        particle[0][0] += particle[1][0]
-        particle[0][1] += particle[1][1]
-        particle[1][1] += 0.2  # Gravidade
-        pygame.draw.circle(screen, (255, 0, 0), [int(particle[0][0]), int(particle[0][1])], 5)'''
-
-# Função para mostrar a tela de ajuda
+# Function to display the help screen
 def show_help(screen):
     help_text = (
-        "Help: \n\n"
-        "1. Aim at the targets and shoot by clicking the left mouse button.\n"
+        ">> Help [?] \n\n"
+        "1. Aim at the targets and shoot by clicking the left\n    mouse button.\n"
         "2. Each hit gives you 10 points.\n"
-        "3. You have 6 bullets, after which you need to reload by clicking the right mouse button.\n"
+        "3. You have 6 bullets, after which you need to reload\n    by clicking the right mouse button.\n"
         "4. The game lasts 15 seconds, try to get the highest score!\n"
         "5. Press ESC to return to the main menu."
     )
@@ -84,13 +64,13 @@ def show_help(screen):
 
         pygame.display.flip()
 
-# Função para mostrar a tela de pontuações
+# Function to display the scores screen
 # -- next update maybe
 
-# Função para salvar a pontuação
+# Function to save the score
 # -- next update maybe
 
-# Função principal do jogo
+# Main game function
 def main_game(screen):
     target_rect = target_image.get_rect()
     aim_rect = aim_image.get_rect()
@@ -109,13 +89,22 @@ def main_game(screen):
 
     pygame.mouse.set_visible(False)
 
+    # Select a random music track
+    musics = ["assets/bg2.mp3", "assets/bg3.mp3", "assets/bg4.mp3"]
+    random_music = random.choice(musics)
+
+    # Play the music
+    pygame.mixer.music.load(random_music)
+    pygame.mixer.music.set_volume(0.7)
+    pygame.mixer.music.play(-1)
+
     def new_target():
         target_rect.x = random.randint(0, screen_width - target_rect.width)
         target_rect.y = random.randint(0, max(0, screen_height - target_rect.height))
 
     new_target()
 
-    #Shot particles
+    # Shot particles
     particle_system = shot_particles.ParticleSystem()
 
 
@@ -150,13 +139,14 @@ def main_game(screen):
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1 and ammo > 0:
                     particle_system.add_particles(event.pos[0], event.pos[1], 7)
-                    shot_sound.play()
+                    random_shot_sound = random.choice(shot_sounds)
+                    random_shot_sound.play()
                     
                     if target_rect.collidepoint(event.pos):
                         hit_sound.play()
                         score += 10
                         ammo -= 1
-                        # criar partículas de explosão do target
+                        # Add the target explosion particles here
                         new_target()
                     else:
                         ammo -= 1
@@ -176,46 +166,75 @@ def main_game(screen):
 
     pygame.quit()
 
-# Função para o menu principal
+# Function for the main menu
 def main_menu(screen):
     background_position = [0, 0] 
     running = True
     logo_image = pygame.image.load('assets/logo_lourifps.png').convert_alpha()
     logo_rect = logo_image.get_rect()
-    logo_rect.center = (screen_width // 2, 200)  # Posição do logotipo
+    logo_rect.center = (screen_width // 2, 200)
 
     pygame.mouse.set_visible(1)
+
+    music_menu = 'assets/bg1.mp3'
+    pygame.mixer.music.load(music_menu)
+    pygame.mixer.music.play(-1)
+
+    # Definição dos botões
+    button_width = 250
+    button_height = 50
+    button_radius = 10
+    button_color = (255, 255, 255)
+    button_hover_color = (200, 200, 200)
+    button_text_color = (0, 0, 0)
+
+    new_game_button = pygame.Rect(0, 0, button_width, button_height)
+    new_game_button.center = (screen_width // 2, 400)
+    help_button = pygame.Rect(0, 0, button_width, button_height)
+    help_button.center = (screen_width // 2, 500)
+    quit_button = pygame.Rect(0, 0, button_width, button_height)
+    quit_button.center = (screen_width // 2, 600)
 
     while running:
         screen.fill(black)
 
         title_text = font.render("FPS Game", True, white)
-        new_game_text = font.render("New Game", True, white)
-        help_text = font.render("Help", True, white)
-        quit_text = font.render("Quit", True, white)
 
-        screen.blit(background_image, background_position) #Parallax
-        screen.blit(logo_image, logo_rect)  # Adiciona o logotipo
+        screen.blit(background_image, background_position) #Parallax not working yet!!!
+        screen.blit(logo_image, logo_rect)
         screen.blit(title_text, (screen_width // 2 - title_text.get_width() // 2, 300))
-        screen.blit(new_game_text, (screen_width // 2 - new_game_text.get_width() // 2, 400))
-        screen.blit(help_text, (screen_width // 2 - help_text.get_width() // 2, 500))
-        screen.blit(quit_text, (screen_width // 2 - quit_text.get_width() // 2, 600))
-        
-        
+
+        # Button definitions
+        for button, text, y in [(new_game_button, "New Game", 400), (help_button, "Help", 500), (quit_button, "Quit", 600)]:
+            if button.collidepoint(pygame.mouse.get_pos()):
+                pygame.draw.rect(screen, button_hover_color, (button.x, button.y, button.width, button.height), border_radius=button_radius)
+                button_width_scaled = int(button_width * 1.1)
+                button_height_scaled = int(button_height * 1.1)
+                button_scaled = pygame.Rect(0, 0, button_width_scaled, button_height_scaled)
+                button_scaled.center = (screen_width // 2, y)
+                pygame.draw.rect(screen, button_hover_color, button_scaled, border_radius=button_radius)
+            else:
+                pygame.draw.rect(screen, button_color, (button.x, button.y, button.width, button.height), border_radius=button_radius)
+
+            text_surface = font.render(text, True, button_text_color)
+            text_rect = text_surface.get_rect(center=(button.centerx, button.centery))
+            screen.blit(text_surface, text_rect)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if new_game_text.get_rect(center=(screen_width // 2, 400)).collidepoint(event.pos):
+                if new_game_button.collidepoint(event.pos):
                     main_game(screen)
-                elif help_text.get_rect(center=(screen_width // 2, 500)).collidepoint(event.pos):
+                elif help_button.collidepoint(event.pos):
                     show_help(screen)
-                elif quit_text.get_rect(center=(screen_width // 2, 600)).collidepoint(event.pos):
+                elif quit_button.collidepoint(event.pos):
                     running = False
+                    pygame.quit()
+                    sys.exit()
 
         pygame.display.flip()
 
-# Execução do menu principal
+# Execute the main menu
 main_menu(screen)
 pygame.quit()
